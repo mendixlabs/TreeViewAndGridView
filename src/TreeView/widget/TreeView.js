a450be2d894c88fcc38b19f70c7a473826b8dc8d
Andries Smit <mr.andries.smit@gmail.com>
2017-06-20 16:20:19 +0200

    Remove duplicate trigger onSelect
    The onSelect was also fired from `saveAndFireSelection`

--- src/TreeView/widget/TreeView.js
+++ src/TreeView/widget/TreeView.js
@@ -371,9 +371,6 @@ require([
                 dojo.forEach(this.actions, function (action) {
                     action.updateToSelection();
                 });
-
-                //fire the on select event
-                this.onSelect(renderNode);
             }
         },
 
